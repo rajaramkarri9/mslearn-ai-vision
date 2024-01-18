d8ed084847a1a90443731d11e2e0634b816864d2
Rajaramkarri9 <rajaramkarri9@example.com>
2024-01-18 10:30:51 -0600

    Chnages

--- Labfiles/05-ocr/Python/read-text/read-text.py
+++ Labfiles/05-ocr/Python/read-text/read-text.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageDraw
 from matplotlib import pyplot as plt
 
 # Import namespaces
-
+import azure.ai.vision as sdk
 
 def main():
 
@@ -18,7 +18,7 @@ def main():
         ai_key = os.getenv('AI_SERVICE_KEY')
 
         # Authenticate Azure AI Vision client
-        
+        cv_client = sdk.VisionServiceOptions(ai_endpoint, ai_key)        
         
 
 
@@ -40,7 +40,71 @@ def GetTextRead(image_file):
     print('\n')
 
     # Use Analyze image function to read text in image
+    print('Reading text in {}\n'.format(image_file))
 
+    analysis_options = sdk.ImageAnalysisOptions()
+
+    features = analysis_options.features = (
+        # Specify the features to be retrieved
+        sdk.ImageAnalysisFeature.TEXT
+
+    )
+
+    # Get image analysis
+    image = sdk.VisionSource(image_file)
+
+    image_analyzer = sdk.ImageAnalyzer(cv_client, image, analysis_options)
+
+    result = image_analyzer.analyze()
+
+    if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+
+        # Get image captions
+        if result.text is not None:
+            print("\nText:")
+
+            # Prepare image for drawing
+            image = Image.open(image_file)
+            fig = plt.figure(figsize=(image.width/100, image.height/100))
+            plt.axis('off')
+            draw = ImageDraw.Draw(image)
+            color = 'cyan'
+
+            for line in result.text.lines:
+                # Return the text detected in the image
+                print(line.content)    
+
+                drawLinePolygon = True
+
+                r = line.bounding_polygon
+                bounding_polygon = ((r[0], r[1]),(r[2], r[3]),(r[4], r[5]),(r[6], r[7]))
+
+                # Return each line detected in the image and the position bounding box around each line
+                print(" Line: '{}', Bounding Polygon: {}".format(line.content, bounding_polygon))
+
+
+                # Return each word detected in the image and the position bounding box around each word with the confidence level of each word
+                for word in line.words:
+                    r = word.bounding_polygon
+                    bounding_polygon = ((r[0], r[1]),(r[2], r[3]),(r[4], r[5]),(r[6], r[7]))
+                    print("  Word: '{}', Bounding Polygon: {}, Confidence: {}".format(word.content, bounding_polygon,word.confidence))
+
+                    # Draw word bounding polygon
+                    drawLinePolygon = False
+                    draw.polygon(bounding_polygon, outline=color, width=3)
+
+
+                # Draw line bounding polygon
+                if drawLinePolygon:
+                    draw.polygon(bounding_polygon, outline=color, width=3)
+
+
+            # Save image
+                plt.imshow(image)
+                plt.tight_layout(pad=0)
+                outputfile = 'text.jpg'
+                fig.savefig(outputfile)
+                print('\n  Results saved in', outputfile)
 
 
 
--- Labfiles/06-video-indexer/get-videos.ps1
+++ Labfiles/06-video-indexer/get-videos.ps1
@@ -1,7 +1,7 @@
-$account_id="YOUR_ACCOUNT_ID"
-$api_key="YOUR_API_KEY"
+$account_id="17f74107-04e7-473c-a8dc-a94680b100f2"
+$api_key="ad9203e5fa34490496852c3d794ecc06"
 $location="trial"
-
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
 # Call the AccessToken method with the API key in the header to get an access token
 $token = Invoke-RestMethod -Method "Get" -Uri "https://api.videoindexer.ai/auth/$location/Accounts/$account_id/AccessToken" -Headers @{'Ocp-Apim-Subscription-Key' = $api_key}
 
